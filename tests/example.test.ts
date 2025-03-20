7c4e55c131a7c81f28921935de7fb9b3b540cc27
sifatfaysald <sifatfaysald@gmail.com>
2025-03-20 22:32:45 +0600

    update playwright test

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('Example page title test', async ({ page }) => {
-    await page.goto('https://example.com');
-    await expect(page).toHaveTitle('Example Domain');
+test('page title test', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+    await page.getByRole('link', { name: 'Get started' }).click();
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
