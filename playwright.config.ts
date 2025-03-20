d572fc3c37361cc651d49d8cb28b288f15fea76b
swesifat <swesifat@gmail.com>
2025-03-20 20:59:53 +0600

    config playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+    testDir: './tests',
     use: {
         headless: false,
+        browserName: 'chromium',
+        channel: 'chrome',
+        viewport: { width: 1920, height: 1080 },
     },
     reporter: [['html', { outputFolder: 'playwright-report' }]],
 });
